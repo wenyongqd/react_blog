d805e885fe31b29b77a082e2c9d9b2acf527d349
wenyongqd <wenyongqd@gmail.com>
2020-02-04 23:14:29 -0500

    update header

---
+++ components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../static/style/components/header.css'
+import {Row, Col, Menu, Icon} from 'antd'
+
+const Header = () => (
+    <div className="header">
+        <Row type="flex" justify="center">
+            <Col xs={24} sm={24} md={10} lg={15} xl={12}>
+                <span className="header-logo">Yong's blog</span>
+                <span className="header-txt">focusing on coding...</span>
+            </Col>
+            <Col className="memu-div" xs={0} sm={0} md={14} lg={8} xl={6}>
+                <Menu mode="horizontal">
+                    <Menu.Item key="home">
+                        <Icon type="home"/>
+                        Home
+                    </Menu.Item>
+                    <Menu.Item key="video">
+                        <Icon type="youtube" />
+                        Video
+                    </Menu.Item>
+                    <Menu.Item key="life">
+                        <Icon type="smile" />
+                        Life
+                    </Menu.Item>
+                </Menu>
+            </Col>
+        </Row>
+    </div>
+)
+
+export default Header
