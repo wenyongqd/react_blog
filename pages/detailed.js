93a6b87705e0c48c58b919bcfc0cf6ccce49108b
wenyongqd <wenyongqd@gmail.com>
2020-02-06 23:19:37 -0500

    update service

--- pages/detailed.js
+++ pages/detailed.js
@@ -1,23 +1,106 @@
 import React from 'react'
 import Head from 'next/head'
-import {Row, Col} from 'antd'
+import {Row, Col, Affix, Icon, Breadcrumb} from 'antd'
 import Header from '../components/Header'
+import Author from '../components/Author'
+import Advert from '../components/Advert'
+import Footer from '../components/Footer'
+import '../static/style/pages/detailed.css'
+import ReactMarkdown from 'react-markdown'
+import MarkNav from 'markdown-navbar'
+import 'markdown-navbar/dist/navbar.css'
 
-const Detailed = () => (
-  <div>
+const Detailed = () => {
+  let markdown='# P01:课程介绍和环境搭建\n' +
+  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+   '**这是加粗的文字**\n\n' +
+  '*这是倾斜的文字*`\n\n' +
+  '***这是斜体加粗的文字***\n\n' +
+  '~~这是加删除线的文字~~ \n\n'+
+  '\`console.log(111)\` \n\n'+
+  '# p02:来个Hello World 初始Vue3.0\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n'+
+  '***\n\n\n' +
+  '# p03:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p04:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '#5 p05:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p06:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p07:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '``` var a=11; ```'
+  return (
+    <div>
     <Head>
       <title>Detailed</title>
     </Head>
     <Header />
     <Row className="comm-main" type="flex" justify="center">
-      <col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
-        left
-      </col>
+      <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+        <div>
+          <div className="bread-div">
+            <Breadcrumb>
+              <Breadcrumb.Item><a href="/">Home</a></Breadcrumb.Item>
+              <Breadcrumb.Item><a href="/">Blog list</a></Breadcrumb.Item>
+              <Breadcrumb.Item>xxxx</Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
+          <div>
+            <div className="detailed-title">
+              How to Learn React in 3 Months
+            </div>
+            <div className="list-icon center">
+              <span><Icon type="calender" />2019-06-28</span>
+              <span><Icon type="folder" />Blog List</span>
+              <span><Icon type="fire" />5373 views</span>
+            </div>
+            <div className="detailed-content">
+              <ReactMarkdown 
+              source={markdown}
+              escapeHtml={false} 
+              />
+            </div>
+          </div>
+        </div>
+      </Col>
       <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4} >
-        right
+        <Author />
+        <Advert />
+        <Affix offsetTop={5}>
+          <div className="detailed-nav comm-box">
+            <div className="nav-title">blog list</div>
+            <MarkNav 
+              className="article-menu"
+              source={markdown}
+              headingTopOffset={0}
+              ordered={false}
+            />
+          </div>
+        </Affix>
+
       </Col>
     </Row>
+    <Footer />
   </div>
-)
+  
+  )
+
+}
 
 export default Detailed
